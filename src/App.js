e7204997ad5e090430b23229c45fa0653e4e8ff1
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-05-09 20:26:19 +0500

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Result from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Salom</h1>
+      <Result/>
     </div>
   );
 }
