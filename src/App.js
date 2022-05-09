f49ab3261bd8af209a3b1614e6d9ece602369e84
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-05-09 19:58:40 +0500

    changed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Deployment is happening!
+        breaking changes!
       </header>
     </div>
   );
