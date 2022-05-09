a23b0f42e84a109e92d7920d5e6fa53cd47ea377
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-05-09 20:07:49 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Added data!
-      </header>
+      <SearchBar placeholder="Search a word..."/>
     </div>
   );
 }
